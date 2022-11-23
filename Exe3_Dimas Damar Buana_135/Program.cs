51b0dfc15da31beb29139f0f05f003f6a6632e2b
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-23 15:32:55 +0700

    add class for Nodes

--- Exe3_Dimas Damar Buana_135/Program.cs
+++ Exe3_Dimas Damar Buana_135/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Exe3_Dimas_Damar_Buana_135
 {
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
     internal class Program
     {
         static void Main(string[] args)
