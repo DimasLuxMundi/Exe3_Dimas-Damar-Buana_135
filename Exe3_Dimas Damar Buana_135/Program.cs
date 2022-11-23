4704b22156d4882c127a136556f39854cb69ba13
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-23 15:43:37 +0700

    add method if the list is empty or not

--- Exe3_Dimas Damar Buana_135/Program.cs
+++ Exe3_Dimas Damar Buana_135/Program.cs
@@ -34,6 +34,13 @@ namespace Exe3_Dimas_Damar_Buana_135
             else
                 return false;//return false if the node is not found
         }
+        public bool listEmpty()
+        {
+            if (LAST == null)
+                return true;
+            else
+                return false;
+        }
 
     }
     internal class Program
