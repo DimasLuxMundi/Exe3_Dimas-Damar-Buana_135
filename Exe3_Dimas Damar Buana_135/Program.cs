764223ad80627dafbafe2c708391b7102db26049
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-23 16:20:47 +0700

    add menu

--- Exe3_Dimas Damar Buana_135/Program.cs
+++ Exe3_Dimas Damar Buana_135/Program.cs
@@ -67,13 +67,80 @@ namespace Exe3_Dimas_Damar_Buana_135
             else
                 Console.WriteLine("\nThe first record in the list is: \n\n " + LAST.next.rollNumber + "  " + LAST.next.name);
         }
-
-
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            CircularList obj = new CircularList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\n Menu" +
+                        "\n 1. View all the records in the list" +
+                        "\n 2. Search for a record in the list" +
+                        "\n 3. Display the first record in the list" +
+                        "\n 4. Exit" +
+                        
+                        "\n Enter your choice (1-4): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("Enter the roll number of the students whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number " + curr.rollNumber);
+                                    Console.WriteLine("\nName " + curr.name);
+
+
+                                }
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.firstNode();
+                            }
+                            break;
+                        case '4':
+                            {
+                                return;
+                            }                        
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+
+            }
         }
+
+        
+            
+
+
     }
+
 }
